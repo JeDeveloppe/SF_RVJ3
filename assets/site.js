5b12645eedc8cc7ee7841245ed9405190b8dc6aa
JeDeveloppe <jedeveloppe.contact@gmail.com>
2024-01-07 16:50:23 +0100

    ok

--- assets/site.js
+++ assets/site.js
@@ -14,6 +14,7 @@ import './bootstrap.js';
 
 require('bootstrap');
 
+// Navbar effect
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -27,6 +28,47 @@ function scrollFunction() {
     document.getElementById("logoTop").style.scale = 1;
   }
 }
+// End navbar effect
 
+// SEO skill
 document.querySelector('meta[property~="og:title"]').setAttribute("content",document.title);
 document.querySelector('meta[name="description"]').setAttribute("content",document.querySelector('meta[property~="og:description"]').content);
+// End SEO skill
+
+// CookieBand effect 
+let cookieChecked = getCookie('Refaitesvosjeux');
+
+if(cookieChecked !== "cookieChecked"){
+  let cookieHeadBand = document.querySelector(".cookiealert");
+  let cookieHeadBandButton = document.querySelector("#cookieHeadBandButton");
+  cookieHeadBand.classList.add('show');
+
+  cookieHeadBandButton.addEventListener('click', () => {
+    cookieHeadBand.classList.remove('show');
+    setCookie('Refaitesvosjeux','cookieChecked');
+  })
+}
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+// End CookieBand effect 
--- assets/toast.js
+++ assets/toast.js
@@ -1,7 +1,5 @@
 const liveToasts = document.getElementsByClassName("liveToast");
 
-console.log(liveToasts);
-
 Array.from(liveToasts).forEach((element) => 
     setTimeout(() => {
         element.classList.add('swing-out-top-bck');
