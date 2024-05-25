c7334d83e366f613e2568df189e5e707c5365145
JeDeveloppe <jedeveloppe.contact@gmail.com>
2024-05-25 20:17:01 +0200

    Update DepartmentCrudController

--- src/Controller/Admin/EasyAdmin/DepartmentCrudController.php
+++ src/Controller/Admin/EasyAdmin/DepartmentCrudController.php
@@ -3,12 +3,12 @@
 namespace App\Controller\Admin\EasyAdmin;
 
 use App\Entity\Department;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class DepartmentCrudController extends AbstractCrudController
 {
