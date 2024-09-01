b68c80115161b7e7a2a1acc78a412fd334345aa7
JeDeveloppe <jedeveloppe.contact@gmail.com>
2024-09-01 22:00:37 +0200

    ok

--- src/Command/InitDataBase2.php
+++ src/Command/InitDataBase2.php
@@ -77,35 +77,35 @@ class InitDataBase2 extends Command
         $io = new SymfonyStyle($input,$output);
 
         //on importe les partenaires
-        // $this->partnerService->importPartenaires($io);
+        $this->partnerService->importPartenaires($io);
 
-        // //on importe les adresses (facturation et livraison)
-        // $this->adresseService->importAdresses($io);
+        //on importe les adresses (facturation et livraison)
+        $this->adresseService->importAdresses($io);
 
-        // //on creer le nombre de joueurs
+        //on creer le nombre de joueurs
         $this->playerService->addplayers($io);
 
         //on cree les temps de jeux
-        //$this->durationOfGameService->addDurations($io);
+        $this->durationOfGameService->addDurations($io);
 
-        // //on importe les boites
-        // $this->boiteService->importBoites($io);
+        //on importe les boites
+        $this->boiteService->importBoites($io);
 
-        // //on genere les editeurs de facon distinct
-        // $this->editorService->addEditorsInDatabase($io);
+        //on genere les editeurs de facon distinct
+        $this->editorService->addEditorsInDatabase($io);
 
-        // //on importe le detail des boites
-        // $this->boiteService->importPieces($io);
+        //on importe le detail des boites
+        $this->boiteService->importPieces($io);
 
-        // //on cree utilisateur undefini, adresse de retrait COOP, methodes de retrait
-        // $this->userService->createUndefinedUser($io);
-        // $this->boiteService->createUndefinedBoite($io);
-        // $this->adresseService->createRetredAddress($io);
-        // $this->shippingMethodService->createShippingMethode($io);
-        // $this->collectionPointService->addCollectionPoint($io);
+        //on cree utilisateur undefini, adresse de retrait COOP, methodes de retrait
+        $this->userService->createUndefinedUser($io);
+        $this->boiteService->createUndefinedBoite($io);
+        $this->adresseService->createRetredAddress($io);
+        $this->shippingMethodService->createShippingMethode($io);
+        $this->collectionPointService->addCollectionPoint($io);
 
-        // //on cree les conditions des occasions
-        // $this->occasionService->addConditions($io);
+        //on cree les conditions des occasions
+        $this->occasionService->addConditions($io);
 
         //on cree les MOYENS DE PAIEMENT
         $this->meansOffPayementService->addMoyens($io);
