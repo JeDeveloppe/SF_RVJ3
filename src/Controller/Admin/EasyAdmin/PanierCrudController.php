a2ff80910e9cb1077babc5cf50fe5f5c4336e829
Je Développe <JeDeveloppe@users.rw.github.com>
2024-11-30 18:56:43 +0100

    CORRECTIONS - affichage durée conversation du panier - admin PanierCrud => plus possible de voir le détail

--- src/Controller/Admin/EasyAdmin/PanierCrudController.php
+++ src/Controller/Admin/EasyAdmin/PanierCrudController.php
@@ -57,7 +57,6 @@ class PanierCrudController extends AbstractCrudController
             ->remove(Crud::PAGE_INDEX, Action::EDIT)
             ->remove(Crud::PAGE_INDEX, Action::NEW)
             ->remove(Crud::PAGE_DETAIL, Action::DELETE)
-            ->remove(Crud::PAGE_DETAIL, Action::EDIT)
-            ->add(Crud::PAGE_INDEX, Action::DETAIL);        
+            ->remove(Crud::PAGE_DETAIL, Action::EDIT);        
     }
 }
